ae02ac77c7a30884e1069183a5ca497fe9fc64b0
Guillaume Crégut <gcregut@free.fr>
2024-08-27 23:11:45 +0200

    creating programm part

--- Uppale.dfm
+++ Uppale.dfm
@@ -2,25 +2,26 @@ object FPpale: TFPpale
   Left = 0
   Top = 0
   Caption = 'Programmation du coffret'
-  ClientHeight = 441
-  ClientWidth = 624
+  ClientHeight = 543
+  ClientWidth = 976
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
-  OnClose = FormClose
   OnCreate = FormCreate
   TextHeight = 15
   object PageControl1: TPageControl
     Left = 0
     Top = 0
-    Width = 624
-    Height = 415
-    ActivePage = TSConfig
+    Width = 976
+    Height = 517
+    ActivePage = TSProgrammation
     Align = alClient
     TabOrder = 0
+    ExplicitWidth = 624
+    ExplicitHeight = 415
     object TSConfig: TTabSheet
       Caption = 'Configuration'
       object GBParamsCom: TGroupBox
@@ -140,12 +141,303 @@ object FPpale: TFPpale
     object TSProgrammation: TTabSheet
       Caption = 'Programmation'
       ImageIndex = 1
+      object GBProg: TGroupBox
+        Left = 3
+        Top = 3
+        Width = 473
+        Height = 318
+        Caption = 'R'#233'seau'
+        TabOrder = 0
+        object Label6: TLabel
+          Left = 16
+          Top = 24
+          Width = 112
+          Height = 15
+          Caption = 'Identifiant Wifi (SSID)'
+        end
+        object Label7: TLabel
+          Left = 16
+          Top = 64
+          Width = 41
+          Height = 15
+          Caption = 'Cl'#233' Wifi'
+        end
+        object ESsid: TEdit
+          Left = 144
+          Top = 21
+          Width = 177
+          Height = 23
+          TabOrder = 0
+        end
+        object EWifiPass: TEdit
+          Left = 144
+          Top = 61
+          Width = 281
+          Height = 23
+          TabOrder = 1
+        end
+        object CBDHCP: TCheckBox
+          Left = 16
+          Top = 96
+          Width = 57
+          Height = 17
+          Caption = 'DHCP'
+          Checked = True
+          State = cbChecked
+          TabOrder = 2
+          OnClick = CBDHCPClick
+        end
+        object PNetwork: TPanel
+          Left = 16
+          Top = 119
+          Width = 409
+          Height = 170
+          BevelInner = bvLowered
+          ParentBackground = False
+          TabOrder = 3
+          object Label8: TLabel
+            Left = 16
+            Top = 8
+            Width = 54
+            Height = 15
+            Caption = 'Adresse IP'
+          end
+          object Label9: TLabel
+            Left = 16
+            Top = 45
+            Width = 122
+            Height = 15
+            Caption = 'Masque de sous r'#233'seau'
+          end
+          object Label10: TLabel
+            Left = 16
+            Top = 75
+            Width = 51
+            Height = 15
+            Caption = 'Passerelle'
+          end
+          object Label11: TLabel
+            Left = 16
+            Top = 104
+            Width = 32
+            Height = 15
+            Caption = 'DNS 1'
+          end
+          object Label12: TLabel
+            Left = 16
+            Top = 141
+            Width = 32
+            Height = 15
+            Caption = 'DNS 2'
+          end
+          object MEIpAdress: TMaskEdit
+            Left = 88
+            Top = 8
+            Width = 137
+            Height = 21
+            EditMask = '!099.099.099.099;1;'
+            Font.Charset = DEFAULT_CHARSET
+            Font.Color = clWindowText
+            Font.Height = -12
+            Font.Name = 'Courier'
+            Font.Style = []
+            MaxLength = 15
+            ParentFont = False
+            TabOrder = 0
+            Text = '   .   .   .   '
+            OnExit = MEIpAdressExit
+          end
+          object MESubnet: TMaskEdit
+            Left = 144
+            Top = 43
+            Width = 145
+            Height = 21
+            EditMask = '!099.099.099.099;1;'
+            Font.Charset = DEFAULT_CHARSET
+            Font.Color = clWindowText
+            Font.Height = -12
+            Font.Name = 'Courier'
+            Font.Style = []
+            MaxLength = 15
+            ParentFont = False
+            TabOrder = 1
+            Text = '   .   .   .   '
+            OnExit = MESubnetExit
+          end
+          object MEGateway: TMaskEdit
+            Left = 144
+            Top = 72
+            Width = 145
+            Height = 21
+            EditMask = '!099.099.099.099;1;'
+            Font.Charset = DEFAULT_CHARSET
+            Font.Color = clWindowText
+            Font.Height = -12
+            Font.Name = 'Courier'
+            Font.Style = []
+            MaxLength = 15
+            ParentFont = False
+            TabOrder = 2
+            Text = '   .   .   .   '
+            OnChange = MEGatewayChange
+          end
+          object MEDns1: TMaskEdit
+            Left = 144
+            Top = 101
+            Width = 145
+            Height = 21
+            EditMask = '!099.099.099.099;1;'
+            Font.Charset = DEFAULT_CHARSET
+            Font.Color = clWindowText
+            Font.Height = -12
+            Font.Name = 'Courier'
+            Font.Style = []
+            MaxLength = 15
+            ParentFont = False
+            TabOrder = 3
+            Text = '   .   .   .   '
+            OnChange = MEDns1Change
+          end
+          object MEDns2: TMaskEdit
+            Left = 144
+            Top = 136
+            Width = 145
+            Height = 21
+            EditMask = '!099.099.099.099;1;'
+            Font.Charset = DEFAULT_CHARSET
+            Font.Color = clWindowText
+            Font.Height = -12
+            Font.Name = 'Courier'
+            Font.Style = []
+            MaxLength = 15
+            ParentFont = False
+            TabOrder = 4
+            Text = '   .   .   .   '
+            OnChange = MEDns2Change
+          end
+        end
+      end
+      object GBAppli: TGroupBox
+        Left = 3
+        Top = 343
+        Width = 473
+        Height = 130
+        Caption = 'Application'
+        TabOrder = 1
+        object Label13: TLabel
+          Left = 16
+          Top = 24
+          Width = 99
+          Height = 15
+          Caption = 'Adresse du serveur'
+        end
+        object Label14: TLabel
+          Left = 16
+          Top = 52
+          Width = 89
+          Height = 15
+          Caption = 'Cl'#233' d'#39'application'
+        end
+        object Label15: TLabel
+          Left = 17
+          Top = 96
+          Width = 160
+          Height = 15
+          Caption = 'P'#233'riode de mesure (en heures)'
+        end
+        object MEIpServer: TMaskEdit
+          Left = 135
+          Top = 16
+          Width = 141
+          Height = 21
+          EditMask = '!099.099.099.099;1;'
+          Font.Charset = DEFAULT_CHARSET
+          Font.Color = clWindowText
+          Font.Height = -12
+          Font.Name = 'Courier'
+          Font.Style = []
+          MaxLength = 15
+          ParentFont = False
+          TabOrder = 0
+          Text = '   .   .   .   '
+          OnExit = MEIpServerExit
+        end
+        object EAppKey: TEdit
+          Left = 135
+          Top = 53
+          Width = 186
+          Height = 23
+          TabOrder = 1
+        end
+        object EPeriod: TEdit
+          Left = 196
+          Top = 90
+          Width = 59
+          Height = 23
+          ReadOnly = True
+          TabOrder = 2
+          Text = '1'
+        end
+        object SpinButton1: TSpinButton
+          Left = 256
+          Top = 88
+          Width = 20
+          Height = 25
+          DownGlyph.Data = {
+            0E010000424D0E01000000000000360000002800000009000000060000000100
+            200000000000D800000000000000000000000000000000000000008080000080
+            8000008080000080800000808000008080000080800000808000008080000080
+            8000008080000080800000808000000000000080800000808000008080000080
+            8000008080000080800000808000000000000000000000000000008080000080
+            8000008080000080800000808000000000000000000000000000000000000000
+            0000008080000080800000808000000000000000000000000000000000000000
+            0000000000000000000000808000008080000080800000808000008080000080
+            800000808000008080000080800000808000}
+          TabOrder = 3
+          UpGlyph.Data = {
+            0E010000424D0E01000000000000360000002800000009000000060000000100
+            200000000000D800000000000000000000000000000000000000008080000080
+            8000008080000080800000808000008080000080800000808000008080000080
+            8000000000000000000000000000000000000000000000000000000000000080
+            8000008080000080800000000000000000000000000000000000000000000080
+            8000008080000080800000808000008080000000000000000000000000000080
+            8000008080000080800000808000008080000080800000808000000000000080
+            8000008080000080800000808000008080000080800000808000008080000080
+            800000808000008080000080800000808000}
+          OnDownClick = SpinButton1DownClick
+          OnUpClick = SpinButton1UpClick
+        end
+      end
+      object BReadProd: TButton
+        Left = 520
+        Top = 63
+        Width = 75
+        Height = 25
+        Caption = 'Lecture'
+        TabOrder = 2
+      end
+      object BWriteProg: TButton
+        Left = 520
+        Top = 112
+        Width = 75
+        Height = 25
+        Caption = 'Ecrire'
+        TabOrder = 3
+        OnClick = BWriteProgClick
+      end
+      object Memo1: TMemo
+        Left = 736
+        Top = 35
+        Width = 185
+        Height = 347
+        TabOrder = 4
+      end
     end
   end
   object SBStatus: TStatusBar
     Left = 0
-    Top = 415
-    Width = 624
+    Top = 517
+    Width = 976
     Height = 26
     Panels = <
       item
@@ -157,6 +449,7 @@ object FPpale: TFPpale
         Width = 50
       end>
     ExplicitLeft = 8
-    ExplicitTop = 417
+    ExplicitTop = 519
+    ExplicitWidth = 624
   end
 end
--- Uppale.pas
+++ Uppale.pas
@@ -4,9 +4,10 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
+  System.Classes, Vcl.Graphics, System.Types,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls,
-  System.Win.Registry, StrUtils, System.UITypes;
+  System.Win.Registry, StrUtils, System.UITypes, Vcl.ExtCtrls, Vcl.Mask,
+  Vcl.Samples.Spin;
 
 type
   TFPpale = class(TForm)
@@ -26,19 +27,69 @@ type
     CBStop: TComboBox;
     BValidateParam: TButton;
     SBStatus: TStatusBar;
+    GBProg: TGroupBox;
+    Label6: TLabel;
+    ESsid: TEdit;
+    Label7: TLabel;
+    EWifiPass: TEdit;
+    CBDHCP: TCheckBox;
+    PNetwork: TPanel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    GBAppli: TGroupBox;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    BReadProd: TButton;
+    BWriteProg: TButton;
+    MEIpAdress: TMaskEdit;
+    MESubnet: TMaskEdit;
+    MEGateway: TMaskEdit;
+    MEDns1: TMaskEdit;
+    MEDns2: TMaskEdit;
+    MEIpServer: TMaskEdit;
+    EAppKey: TEdit;
+    EPeriod: TEdit;
+    SpinButton1: TSpinButton;
+    Memo1: TMemo;
     procedure LoadComNumber();
     procedure LoadFromRegistry();
     procedure StoreToRegistry();
+    procedure EnableNetWorkPanel(state: Boolean);
     procedure FormCreate(Sender: TObject);
     procedure BValidateParamClick(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure CBDHCPClick(Sender: TObject);
+    procedure SpinButton1DownClick(Sender: TObject);
+    procedure SpinButton1UpClick(Sender: TObject);
+    procedure ConfigureComPort();
+    function validIpAdress(adress: String): string;
+    procedure MEIpAdressExit(Sender: TObject);
+    procedure MESubnetExit(Sender: TObject);
+    procedure MEGatewayChange(Sender: TObject);
+    procedure MEDns1Change(Sender: TObject);
+    procedure MEDns2Change(Sender: TObject);
+    function CreateProg(): TStringList;
+    procedure MEIpServerExit(Sender: TObject);
+    procedure BWriteProgClick(Sender: TObject);
 
   const
     RegKey = 'Software\Editiel98\CellarManager\';
   private
     { Déclarations privées }
     isValidate: Boolean;
-
+    mesureInterval: Integer;
+    useDHCP: Boolean;
+    serialHasChanged: Boolean;
+    progHasChanged: Boolean;
+    IPCard : String;
+    SubNetCard: String;
+    GatewayCard: String;
+    DNS1Card: String;
+    DNS2Card: String;
+    IpServerCard : String;
   public
     { Déclarations publiques }
   end;
@@ -49,25 +100,228 @@ var
 implementation
 
 {$R *.dfm}
+//TODO: Change ProgHasChanged for wifi and params
+function TFPpale.CreateProg(): TStringList;
+var
+  IpAdress, SubnetMask, DNS1, DNS2, Gateway, SSID, PassWifi, APIKey,
+    IpServer, Period, DateDay, DateMonth,DateYear, DateHour,DateMinute: String;
+  CurrentTime: TDateTime;
+  ConfigArray: TStringList;
+
+begin
+  ConfigArray:= TStringList.create();
+  IpAdress := '';
+  SubnetMask := '';
+  DNS1 := '';
+  Gateway := '';
+  DNS2 := '';
+  SSID:=ESsid.Text;
+  if SSID='' then
+  begin
+    MessageDlg('Le wifi ne doit pas être vide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      raise Exception.Create('Message d''erreur');
+  end;
+  PassWifi:= EWifiPass.Text;
+  if PassWifi='' then
+  begin
+    MessageDlg('Le mot de passe wifi ne doit pas être vide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      raise Exception.Create('Message d''erreur');
+  end;
+  ConfigArray.Add(SSID);
+  ConfigArray.Add(PassWifi);
+
+  if not useDHCP then
+  begin
+    IpAdress := IPCard;
+    SubnetMask := SubNetCard;
+    DNS1 := DNS1Card;
+    Gateway := GatewayCard;
+    DNS2 := DNS2Card;
+    if((IpAdress='') Or (SubnetMask='') Or(Gateway='') or (DNS1='') or(DNS2='')) then
+    begin
+      MessageDlg('Si vous décochez la cse DHCP, tous les champs doivent être remplis', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      raise Exception.Create('Message d''erreur');
+    end;
+    ConfigArray.Add('1');
+    ConfigArray.Add(IpAdress);
+    ConfigArray.Add(SubnetMask);
+    ConfigArray.Add(Gateway);
+    ConfigArray.Add(DNS1);
+    ConfigArray.Add(DNS2);
+  end
+  else
+  begin
+    ConfigArray.Add('0');
+  end;
+    IpServer:= IpServerCard;
+    if IpServer='' then
+    begin
+       MessageDlg('L''adresse du serveur ne doit pas être vide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      raise Exception.Create('Message d''erreur');
+    end;
+    ConfigArray.Add(IpServer);
+    APIKey:=EAppKey.Text;
+    if APIKey='' then
+    begin
+       MessageDlg('La clé ne doit pas être vide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      raise Exception.Create('Message d''erreur');
+    end;
+    ConfigArray.Add(APIKey);
+    Period:= EPeriod.Text;
+    ConfigArray.Add(Period);
+    CurrentTime:= now;
+    DateDay:= FormatDateTime('dd',CurrentTime);
+    DateMonth := FormatDateTime('mm',CurrentTime);
+    DateYear := FormatDateTime('yyyy',CurrentTime);
+    DateHour:= FormatDateTime('hh',CurrentTime);
+    DateMinute:= FormatDateTime('nn',CurrentTime);
+    ConfigArray.Add(DateYear);
+    ConfigArray.Add(DateMonth);
+    ConfigArray.Add(DateDay);
+    ConfigArray.Add(DateHour);
+    ConfigArray.Add(DateMinute);
+    Result:= ConfigArray;
+
+end;
+
+procedure TFPpale.ConfigureComPort;
+begin
+  // TODO: Configure COM port with datas
+end;
+
+function TFPpale.validIpAdress(adress: string): string;
+var
+  firstDigit, secondDigit, thirdDigit, fourthDigit: Integer;
+begin
+try
+  firstDigit := StrToInt(TrimRight(Copy(adress, 0, 3)));
+  secondDigit := StrToInt(TrimRight(Copy(adress, 5, 3)));
+  thirdDigit := StrToInt(TrimRight(Copy(adress, 9, 3)));
+  fourthDigit := StrToInt(TrimRight(Copy(adress, 13, 3)));
+  if ((firstDigit < 0) Or (firstDigit > 255) Or (secondDigit < 0) Or
+    (secondDigit > 255) Or (thirdDigit < 0) Or (thirdDigit > 255) Or
+    (fourthDigit < 0) Or (fourthDigit > 255)) then
+  begin
+    raise EArgumentException.Create('Not a valid IP address.');
+  end;
+  progHasChanged := True;
+  Result := IntToStr(firstDigit)+'.'+IntToStr(secondDigit)+'.'+IntToStr(thirdDigit)+'.'+IntToStr(fourthDigit);
+except
+  raise EArgumentException.Create('Not a valid IP address.');
+end;
+end;
+
+procedure TFPpale.EnableNetWorkPanel(state: Boolean);
+begin
+  PNetwork.Enabled := not state;
+  MEIpAdress.Enabled := not state;
+  MESubnet.Enabled := not state;
+  MEDns1.Enabled := not state;
+  MEDns2.Enabled := not state;
+  MEGateway.Enabled := not state;
+  if state then
+  begin
+    PNetwork.Color := clInactiveCaption;
+  end
+  else
+  begin
+    PNetwork.Color := clBtnFace;
+  end;
+end;
 
 procedure TFPpale.BValidateParamClick(Sender: TObject);
 begin
   StoreToRegistry();
-  // Configure comport
+  ConfigureComPort();
 end;
 
-procedure TFPpale.FormClose(Sender: TObject; var Action: TCloseAction);
+procedure TFPpale.BWriteProgClick(Sender: TObject);
+var
+  prog: TStringList;
+  toto: String;
+  i: Integer;
 begin
+  prog:= TStringList.Create;
   StoreToRegistry();
+  try
+  try
+    prog:= CreateProg();
+   toto:=IntToStr(prog.Count);
+   SBStatus.panels[0].Text:=toto;
+   Memo1.lines.Clear;
+   for i:=0 to prog.Count-1 do
+   begin
+     Memo1.Lines.Add('step '+IntToStr(i)+' '+prog[i]);
+   end;
+  except on E: Exception do
+  begin
+    MessageDlg('La configuration ne peut être créée', TMsgDlgType.mtWarning, mbOKCancel, 0);
+  end;
+
+  end;
+  finally
+
+  end;
+  // prepare datas for protocol
+  
+end;
+
+procedure TFPpale.CBDHCPClick(Sender: TObject);
+begin
+  EnableNetWorkPanel(CBDHCP.Checked);
+  useDHCP := CBDHCP.Checked;
+  progHasChanged:=True;
 end;
 
 procedure TFPpale.FormCreate(Sender: TObject);
 begin
-  isValidate := false;
+ IPCard :='';
+    SubNetCard:='';
+    GatewayCard:='';
+    DNS1Card:='';
+    DNS2Card:='';
+    IpServerCard:='';
+  isValidate := False;
+  serialHasChanged := False;
+  progHasChanged := False;
+  useDHCP := True;
   LoadComNumber();
   LoadFromRegistry();
   isValidate := CBPort.Items.Count > 0;
   BValidateParam.Enabled := isValidate;
+  BReadProd.Enabled := isValidate;
+  BWriteProg.Enabled := isValidate;
+  mesureInterval := 1;
+  EPeriod.Text := IntToStr(mesureInterval);
+  CBDHCP.Checked := useDHCP;
+  EnableNetWorkPanel(useDHCP);
+  if isValidate then
+  begin
+    ConfigureComPort();
+  end;
+end;
+
+procedure TFPpale.SpinButton1DownClick(Sender: TObject);
+begin
+  if mesureInterval > 1 then
+  begin
+    dec(mesureInterval);
+    EPeriod.Text := IntToStr(mesureInterval);
+  end;
+end;
+
+procedure TFPpale.SpinButton1UpClick(Sender: TObject);
+begin
+  if mesureInterval < 24 then
+  begin
+    inc(mesureInterval);
+    EPeriod.Text := IntToStr(mesureInterval);
+  end;
 end;
 
 procedure TFPpale.StoreToRegistry;
@@ -75,6 +329,8 @@ var
   reg: TRegistry;
   openResult: Boolean;
 begin
+  if (not serialHasChanged) and (not progHasChanged) then
+    Exit;
   reg := TRegistry.create(KEY_WRITE);
   reg.RootKey := HKEY_CURRENT_USER;
   try
@@ -84,14 +340,23 @@ begin
       MessageDlg('Sauvegarde impossible', TMsgDlgType.mtWarning, mbOKCancel, 0);
       Exit;
     end;
-    // Store Parity
-    reg.WriteInteger('Parity', CBParity.ItemIndex);
-    // Store BaudRate
-    reg.WriteInteger('Baud', CBBaudRate.ItemIndex);
-    // Store DataSize
-    reg.WriteInteger('Datas', CBDatas.ItemIndex);
-    // Store stop Bit
-    reg.WriteInteger('Stop', CBStop.ItemIndex);
+    if serialHasChanged then
+    begin
+      // Store Parity
+      reg.WriteInteger('Parity', CBParity.ItemIndex);
+      // Store BaudRate
+      reg.WriteInteger('Baud', CBBaudRate.ItemIndex);
+      // Store DataSize
+      reg.WriteInteger('Datas', CBDatas.ItemIndex);
+      // Store stop Bit
+      reg.WriteInteger('Stop', CBStop.ItemIndex);
+      serialHasChanged := False;
+    end;
+    if progHasChanged then
+    begin
+      // TODO: Add config infos, without Wifi pass
+      progHasChanged := False;
+    end;
   finally
     reg.Free;
   end;
@@ -106,7 +371,7 @@ begin
   reg.RootKey := HKEY_CURRENT_USER;
   if reg.KeyExists(RegKey) then
   begin
-    reg.OpenKey(RegKey, false);
+    reg.OpenKey(RegKey, False);
     // Load Parity
     CBParity.ItemIndex := reg.ReadInteger('Parity');
     // Load BaudRate
@@ -119,12 +384,99 @@ begin
   reg.Free;
 end;
 
+procedure TFPpale.MEDns1Change(Sender: TObject);
+begin
+try
+  DNS1Card := validIpAdress(MEDns1.Text);
+except
+   on E: EArgumentException do
+   begin
+      MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MEDns1.Text:='';
+   end;
+end;
+
+end;
+
+procedure TFPpale.MEDns2Change(Sender: TObject);
+begin
+  try
+     DNS2Card := validIpAdress(MEDns2.Text);
+  except
+    on E: EArgumentException do
+    begin
+       MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MEDns2.Text:='';
+    end;
+  end;
+end;
+
+procedure TFPpale.MEGatewayChange(Sender: TObject);
+begin
+try
+  GatewayCard := validIpAdress(MEGateway.Text);
+   except
+    on E: EArgumentException do
+    begin
+       MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MEGateway.Text:='';
+    end;
+  end;
+end;
+
+procedure TFPpale.MEIpAdressExit(Sender: TObject);
+begin
+try
+  IPCard := validIpAdress(MEIpAdress.Text);
+   except
+    on E: EArgumentException do
+    begin
+       MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MEIpAdress.Text:='';
+    end;
+  end;
+
+end;
+
+procedure TFPpale.MEIpServerExit(Sender: TObject);
+begin
+try
+  IpServerCard := validIpAdress(MEIpServer.Text);
+  except
+    on E: EArgumentException do
+    begin
+       MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MEIpServer.Text:='';
+    end;
+  end;
+
+end;
+
+procedure TFPpale.MESubnetExit(Sender: TObject);
+begin
+try
+  SubNetCard := validIpAdress(MEIpAdress.Text);
+   except
+    on E: EArgumentException do
+    begin
+       MessageDlg('L''adresse IP est invalide', TMsgDlgType.mtError,
+      mbOKCancel, 0);
+      MESubnet.Text:='';
+    end;
+  end;
+end;
+
 procedure TFPpale.LoadComNumber;
 var
   reg: TRegistry;
   openResult: Boolean;
   arrayKeys: TStringList;
-  i: integer;
+  i: Integer;
 begin
   reg := TRegistry.create(KEY_READ);
   reg.RootKey := HKEY_LOCAL_MACHINE;
@@ -132,7 +484,7 @@ begin
   begin
     MessageDlg('Key not found', TMsgDlgType.mtWarning, mbOKCancel, 0);
   end;
-  openResult := reg.OpenKey('HARDWARE\DEVICEMAP\SERIALCOMM\', false);
+  openResult := reg.OpenKey('HARDWARE\DEVICEMAP\SERIALCOMM\', False);
   if (not openResult) then
   begin
     MessageDlg('Key not found', TMsgDlgType.mtWarning, mbOKCancel, 0);
